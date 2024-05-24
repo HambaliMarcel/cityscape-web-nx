f4a0bb93cbe6e4f5d37693b92ac1a80f0ec6a566
Hambali Marcel <marcelinohambali@gmail.com>
2024-05-25 02:49:56 +0700

    feat: add document head-start

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="generator" content="Framer c75d380" />
+        <title>City Scape Apartment - Indonesia</title>
+        <meta
+          name="description"
+          content="Discover the perfect blend of comfort and convenience in this spacious 2-bedroom apartment. Located in a prime neighborhood, this well-appointed unit offers a comfortable living space with two bedrooms, a modern bathroom, and a cozy living room."
+        />
+        <link rel="icon" href="/images/favicon.ico" />
+        {/* Add other meta tags and fonts here */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
