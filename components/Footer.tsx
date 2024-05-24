f0368a729ac5e38c2e9ab9214a5c95da0fc7b99b
Hambali Marcel <marcelinohambali@gmail.com>
2024-05-25 02:51:39 +0700

    feat: add foo-head

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center">
+      <p>&copy; 2023 City Scape Apartment. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
