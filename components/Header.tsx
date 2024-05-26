0d71eac10d12997f8769be1d7d66f2ee672bb60b
Hambali Marcel <marcelinohambali@gmail.com>
2024-05-26 19:07:13 +0700

    feat: components Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,15 @@
-const Header = () => {
+import React from 'react';
+
+const Header: React.FC = () => {
   return (
-    <header className="bg-gray-800 text-white p-4">
-      <h1 className="text-xl">City Scape Apartment - Indonesia</h1>
+    <header className="bg-white p-4 shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-xl font-bold">City Scape Apartment - Indonesia</h1>
+        <nav>
+          <a href="#" className="text-gray-700 hover:text-gray-900">Home</a>
+          <a href="#" className="ml-4 text-gray-700 hover:text-gray-900">About</a>
+        </nav>
+      </div>
     </header>
   );
 };
